9fc29601dd75b064567bb7b9b7919cf4983043dd
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-11 05:06:18 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,21 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.joins(boats: {boat_classifications: :classification}).where("classifications.name = ?", "Catamaran")
+  end
+
+  def self.sailors
+    self.joins(boats: {boat_classifications: :classification}).where("classifications.name = ?", "Sailboat").uniq
+  end
+
+  def self.talented_seamen
+    motorboaters = self.joins(boats: {boat_classifications: :classification}).where("classifications.name = ?", "Motorboat").uniq.pluck("captains.id")
+    sailors = self.sailors.pluck("captains.id")
+    self.where(id: motorboaters & sailors)
+  end
+
+  def self.non_sailors
+    self.where(id: Captain.all.pluck(:id).to_a - self.sailors.pluck("captains.id").to_a)
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    Classification.all
+  end
+
+  def self.longest
+    max = self.joins(boats: :boat_classifications).maximum("boats.length")
+    self.joins(boats: :boat_classifications).where("boats.length = ?", max).uniq
+  end
 end
