dda81f5cdaf6af5404cd696e5128deee146f30e8
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-11 04:19:55 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,16 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.joins(boats: {boat_classifications: :classification}).where("classifications.name = ?", "Catamaran")
+  end
+
+  def self.sailors
+    self.joins(boats: {boat_classifications: :classification}).where("classifications.name = ?", "Sailboat").uniq
+  end
+
+  def self.talented_seamen
+    motorboaters = self.joins(boats: {boat_classifications: :classification}).where("classifications.name = ?", "Motorboat").uniq
+    motorboaters.and(self.sailors)
+  end
 end
