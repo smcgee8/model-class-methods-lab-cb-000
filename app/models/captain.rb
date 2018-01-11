e793f1ee48d7198aeaf05717bd5efb70aad99a1c
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-11 04:32:44 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -11,6 +11,6 @@ class Captain < ActiveRecord::Base
 
   def self.talented_seamen
     motorboaters = self.joins(boats: {boat_classifications: :classification}).where("classifications.name = ?", "Motorboat").uniq
-    motorboaters & self.sailors
+    self.find_by_sql "#{motorboaters.to_sql} INTERSECT #{self.sailors.to_sql}"
   end
 end
