ce91f08e9e804e65208f108fdf8ec7272bc5dbdd
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-11 04:51:03 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -10,7 +10,7 @@ class Captain < ActiveRecord::Base
   end
 
   def self.talented_seamen
-    motorboaters = self.joins(boats: {boat_classifications: :classification}).where("classifications.name = ?", "Sailboat").uniq.pluck("captains.id")
+    motorboaters = self.joins(boats: {boat_classifications: :classification}).where("classifications.name = ?", "Motorboat").uniq.pluck("captains.id")
     sailors = self.sailors.pluck("captains.id")
     binding.pry
     self.where(id: motorboaters & sailors)
